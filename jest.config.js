afaccd00add405b3189322897438d8df7803dedb
MasatoMakino <makino.masato.g@gmail.com>
2023-09-10 13:33:19 +0900

    change : type module

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,20 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+const jestConfig = {
   preset: "ts-jest",
   collectCoverageFrom: ["src/**/*.ts"],
   testEnvironment: "jsdom",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.[tj]sx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
+
+export default jestConfig;
